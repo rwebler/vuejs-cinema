ff23e41bb0ee206c8620fe93b61e5c81a35bf4f0
Rodrigo <rwebler@gmail.com>
2017-09-08 16:39:58 -0300

    after lecture 98

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import './style.scss';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() {
+    return this.$root.moment;
+}});
+
+import { checkFilter } from './util/bus';
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() {
+    return this.$root.bus;
+}});
+
+new Vue({
+    'el': '#app',
+    data: {
+        genre: [],
+        time: [],
+        movies: [],
+        moment,
+        day: moment(),
+        bus
+    },
+    components: {
+        MovieList,
+        MovieFilter
+    },
+    created() {
+        this.$http.get('/api').then(response => {
+            this.movies = response.data;
+        });
+        this.$bus.$on('check-filter', checkFilter.bind(this));
+    }
+});
